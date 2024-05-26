9da511fbeeda336b731efb75df176fe644079510
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2024-05-26 15:15:05 +0100

    Exposing the utilities in the package

--- paqarin/__init__.py
+++ paqarin/__init__.py
@@ -1,4 +1,9 @@
 """Paqarin is a package for the generation of synthetic time series."""
 
-from .adapters import sdv_adapter, synthcity_adapter, ydata_adapter  # noqa: F401
+from .adapters import (
+    sdv_adapter,
+    synthcity_adapter,
+    ydata_adapter,
+)  # noqa: F401
 from .metrics import multivariate_metrics, univariate_metrics  # noqa: F401
+from .utils import data_plots, data_utils  # noqa: F401
