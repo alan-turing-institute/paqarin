3e4e76d00ee2b933bea97905b955e9940f1e459e
Andrew Elliott <ande.elliott@gmail.com>
2024-06-07 10:27:06 +0100

    adding new adapters

--- paqarin/generators/__init__.py
+++ paqarin/generators/__init__.py
@@ -3,3 +3,4 @@
 from .doppleganger import DoppleGangerGenerator, DoppleGanGerParameters  # noqa: F401
 from .par import ParGenerator, ParParameters  # noqa: F401
 from .timegan import TimeGanGenerator, TimeGanParameters  # noqa: F401
+from .timevae import TimeVaeGenerator, TimeVaeParameters
